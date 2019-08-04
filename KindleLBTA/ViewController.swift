885cf35d5c202577f310f30d218f261ace3e19c7
Myo Thura Zaw <mtrzmyothurazaw@gmail.com>
2019-08-04 14:59:12 +0630

    Initial Commit

---
+++ KindleLBTA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KindleLBTA
+//
+//  Created by Myo Thura Zaw on 8/4/19.
+//  Copyright © 2019 Myo Thura Zaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
